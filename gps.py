a7523a56763ace1dd5810e91b2efa428c291656c
XeNoCrAtEz <joydamanik@gmail.com>
2022-04-25 07:49:15 +0100

    added support for GPS using MAVLink

--- gps.py
+++ gps.py
@@ -1,6 +1,8 @@
 import serial
 import threading
 import pynmea2
+from time import sleep
+from dronekit import connect
 
 class GPS:
     """
@@ -10,8 +12,13 @@ class GPS:
 
     Attributes
     ----------
+    isUsingMAVLink : bool
+        True if GPS reading is gained from Flight Controller at the specified serial port
+        False if GPS module is directly connected to the specified serial port
     port : str
         Specifies the path to Serial port filestream
+    UAV : dronekit.Vehicle
+        The UAV's dronekit Vehicle Object
     ser : serial.Serial()
         Serial object for communicating with the GPS
         (default baudrate is 9600, default timeout is 0.5)
@@ -33,18 +40,28 @@ class GPS:
         returns the latest "lat, lng" received from the GPS
     """
 
-    def __init__(self, port="/dev/serial0") -> None:
+    def __init__(self, isUsingMAVLink: bool = True, port: str ="/dev/serial0") -> None:
         """
         Parameters
         ----------
+        isUsingMAVLink : bool
+            - Input True if GPS reading is gained from Flight Controller at the 
+              specified serial port (default, baudrate=57600).
+            - Input False if GPS module is directly connected to the specified serial port
         port : str
             Specifies the path to Serial port filestream
             (default is Serial0 => "/dev/serial0")
         """
 
-        self.port = port
-        self.ser = serial.Serial(self.port, baudrate=9600, timeout=0.5)
+        self.isUsingMAVLink = isUsingMAVLink
 
+        self.port = port
+
+        if isUsingMAVLink:
+            self.UAV = connect(self.port, baud=57600, wait_ready=True)
+        else:
+            self.ser = serial.Serial(self.port, baudrate=9600, timeout=0.5)
+            
         self.lat, self.lng = 0, 0
 
         self.GPSlock = threading.Lock()
@@ -55,16 +72,22 @@ class GPS:
         """
         Get the GPS latitude and longitude data and update the result.
         """
-
-        while True:
-            # get readline until "$GNRMC"
-            newdata = str(self.ser.readline(), errors='ignore')
-            while newdata[0:6] != "$GNRMC":
+        if self.isUsingMAVLink:
+            while True:
+                with self.GPSlock:
+                    self.lat = self.UAV.location.global_relative_frame.lat
+                    self.lng = self.UAV.location.global_relative_frame.lon
+                    sleep(1)
+        else:
+            while True:
+                # get readline until "$GNRMC"
                 newdata = str(self.ser.readline(), errors='ignore')
+                while newdata[0:6] != "$GNRMC":
+                    newdata = str(self.ser.readline(), errors='ignore')
 
-            newmsg = pynmea2.parse(newdata)
-            with self.GPSlock:
-                self.lat, self.lng = newmsg.latitude, newmsg.longitude
+                newmsg = pynmea2.parse(newdata)
+                with self.GPSlock:
+                    self.lat, self.lng = newmsg.latitude, newmsg.longitude
 
     def get_lat_lng(self) -> list:
         """
