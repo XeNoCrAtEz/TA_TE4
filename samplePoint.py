2f5a1226c377226f32f05567131a4d3614a3ab93
XeNoCrAtEz <joydamanik@gmail.com>
2022-04-25 05:34:15 +0100

    removed unused variable and code cleaning

--- samplePoint.py
+++ samplePoint.py
@@ -33,18 +33,18 @@ pointNum = int(sys.argv[1])
 updateTime = int(sys.argv[2])
 samplingTimeout = int(sys.argv[3])
 
+
 print("Initialize PIR system...", end="")
 
 # PIR pin settings
 #      PIR 1  2  3  4   5   6   7   8   9   10
 pin_PIR = (2, 3, 4, 17, 27, 22, 10, 24, 23, 18)
-# delta theta for calculating AoA
-deltaTheta = 360 / len(pin_PIR)
 # PIR system object
 PIRsys = PIR(pin_PIR, 10, 360, updateTime)
 
 if PIRsys.PIRSamplingThread.is_alive(): print("\u001b[32mSuccess\u001b[0m")
 else: raise RuntimeError("PIR sampling thread failed to run!")
+
 
 print("Initialize GPS system...", end="")
 
@@ -53,6 +53,7 @@ GPSsys = GPS()
 
 if GPSsys.GPSSamplingThread.is_alive(): print("\u001b[32mSuccess\u001b[0m")
 else: raise RuntimeError("GPS sampling thread failed to run!")
+
 
 csvUpdateTime = time() + updateTime
 samplingTime = time() + samplingTimeout     # sample this point for 60 seconds
