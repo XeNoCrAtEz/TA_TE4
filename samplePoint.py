5bb9ee81d6d1c5754c102791458438139464fd15
XeNoCrAtEz <joydamanik@gmail.com>
2022-06-07 10:24:56 +0700

    changed pin number

--- samplePoint.py
+++ samplePoint.py
@@ -38,7 +38,7 @@ print("Initialize PIR system...", end="")
 
 # PIR pin settings
 #      PIR 1  2  3  4   5   6   7   8   9   10
-pin_PIR = (2, 3, 4, 17, 27, 22, 10, 24, 23, 18)
+pin_PIR = (4, 17, 27, 22, 10, 11, 0, 24, 23, 18)
 # PIR system object
 PIRsys = PIR(pin_PIR, 10, 360, updateTime)
 
